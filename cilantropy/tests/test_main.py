695728a1f9f81e9cc8c5a7e8f6e2ad3fa6fe91db
Igor Tkachenko <foozzione@gmail.com>
2020-07-13 18:15:21 +0300

    fixed tests

--- cilantropy/tests/test_main.py
+++ cilantropy/tests/test_main.py
@@ -2,7 +2,7 @@ import sys
 sys.path.insert(0, '.')
 
 import unittest
-from cilantropy import main
+import cilantropy.cilantropy
 import xmlrpc.client
 import pkg_resources as _pkg_resources
 
@@ -10,20 +10,20 @@ import pkg_resources as _pkg_resources
 class TestMain(unittest.TestCase):
 
     def test_crumb_params_true(self):
-        crumb = main.Crumb(title='TestCrumb',
+        crumb = cilantropy.cilantropy.Crumb(title='TestCrumb',
                            href='#testcrumb')
         self.assertEqual(crumb.title, 'TestCrumb')
         self.assertEqual(crumb.href, '#testcrumb')
 
     def test_pypi_proxy_instance_true(self):
-        proxy = main.get_pypi_proxy()
+        proxy = cilantropy.cilantropy.get_pypi_proxy()
         self.assertTrue(isinstance(proxy, xmlrpc.client.ServerProxy))
 
     def test_get_shared_data_instance_true(self):
-        data = main.get_shared_data()
+        data = cilantropy.cilantropy.get_shared_data()
         self.assertTrue(isinstance(data, dict))
         self.assertEqual(sorted(data.keys()), ['distributions', 'pypi_update_cache'])
 
     def test_get_pkg_res_instance_true(self):
-        pkg = main.get_pkg_res()
+        pkg = cilantropy.cilantropy.get_pkg_res()
         self.assertEqual(pkg, _pkg_resources)

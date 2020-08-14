f495c41734afafdfb10ab0ddf7cc47f891b76cec
Igor Tkachenko <foozzione@gmail.com>
2020-08-14 16:50:21 +0300

    add missing requirements

--- cilantropy/dist_worker.py
+++ cilantropy/dist_worker.py
@@ -14,58 +14,58 @@ from .helpers import check_pypi_stable_version
 
 
 def main_worker(cmd):
-	""" 
-	main worker for run subproccess and return result or error
+    """
+    main worker for run subproccess and return result or error
 
-	:param cmd: full command for run	
-	:return: result string and status error
-	"""	
+    :param cmd: full command for run
+    :return: result string and status error
+    """
 
-	try:
-		cmd_response = subprocess.run(
-			cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True
-		)
-	except subprocess.CalledProcessError as e:
-		return "The pip command did not succeed: {stderr}".format(
-				stderr=e.stderr.decode("utf-8")
-			), True
-		
-	return cmd_response.stdout.decode("utf-8").replace(" "*6,"\n"), False
+    try:
+        cmd_response = subprocess.run(
+            cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True
+        )
+    except subprocess.CalledProcessError as e:
+        return (
+            "The pip command did not succeed: {stderr}".format(
+                stderr=e.stderr.decode("utf-8")
+            ),
+            True,
+        )
 
+    return cmd_response.stdout.decode("utf-8").replace(" " * 6, "\n"), False
 
 
 class Updater:
-	""" class constructor for Updater
+    """class constructor for Updater
 
-	:param dist_name: distribution name
-	"""	
-	
-	def __init__(self, dist_name):
-		self.pip3_cmd = "pip3"
-		self.pip_upgrade = "--upgrade"
-		self.pip_install = "install"
-		self.pip_user_arg = "--user"
+    :param dist_name: distribution name
+    """
 
-		self.dist_name = dist_name
+    def __init__(self, dist_name):
+        self.pip3_cmd = "pip3"
+        self.pip_upgrade = "--upgrade"
+        self.pip_install = "install"
+        self.pip_user_arg = "--user"
 
+        self.dist_name = dist_name
 
-	def upgrade(self):
-		""" Just build command string for update package
+    def upgrade(self):
+        """Just build command string for update package
 
-		:return: Return result `main_worker`
-		"""
+        :return: Return result `main_worker`
+        """
 
-		version = check_pypi_stable_version(self.dist_name)
-		if not version:
-			version = ''
+        version = check_pypi_stable_version(self.dist_name)
+        if not version:
+            version = ""
 
-		cmd = "{pip3_cmd} {pip_install} {dist_name}=={stable} {pip_upgrade}".format(
-			pip3_cmd=self.pip3_cmd, pip_install=self.pip_install,
-			dist_name=self.dist_name, pip_upgrade=self.pip_upgrade,
-			stable=version
-		)		
+        cmd = "{pip3_cmd} {pip_install} {dist_name}=={stable} {pip_upgrade}".format(
+            pip3_cmd=self.pip3_cmd,
+            pip_install=self.pip_install,
+            dist_name=self.dist_name,
+            pip_upgrade=self.pip_upgrade,
+            stable=version,
+        )
 
-		return main_worker(cmd)
-
-
-
+        return main_worker(cmd)
